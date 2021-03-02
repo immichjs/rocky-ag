f7dcf3f72af7709e4db0203edf2c1bb454cd269d
Michel França <fr.mich17@gmail.com>
2021-03-02 16:49:35 -0300

    Correção de alguns bugs causados no css, e refatoração das funções criadas no js

--- javascript/nav.js
+++ javascript/nav.js
@@ -4,20 +4,24 @@ const btnMenu = document.querySelector('.hd__btn-menu')
 const menuList = document.querySelector('.hd__nav___list')
 const arrowServices = document.querySelector('.hd__nav___list____services')
 const arrowDifferentials = document.querySelector('.hd__nav___list____differentials')
+
+const listItems = document.querySelectorAll('.hd__nav___list____item');
 const arrows = document.querySelectorAll('.hd__nav___list____item_____arrow')
+
+console.log(listItems)
 
 // Executando a manipulação desses elementos.
 // Quando um evento de click for acionado, ele verifica se essas classes estão contidas nos elementos a ser testados.
 // Caso tenha, ele tira, caso contrário ele coloca essas classes.
 btnMenu.addEventListener('click', () => {
-        navMenu.classList.toggle('move')
-        btnMenu.classList.toggle('open')
+    navMenu.classList.toggle('move')
+    btnMenu.classList.toggle('open')
 })
 
-arrows[0].addEventListener('click', () => {
-    arrowServices.classList.toggle('display-visible')
-})
+const arrowServicesFunc = () => arrowServices.classList.toggle('display-visible')
+const arrowDifferentialsFunc = () => arrowDifferentials.classList.toggle('display-visible')
 
-arrows[1].addEventListener('click', () => {
-    arrowDifferentials.classList.toggle('display-visible')
-})
+listItems[2].addEventListener('click', arrowServicesFunc)
+listItems[3].addEventListener('click', arrowDifferentialsFunc)
+arrows[0].addEventListener('click', arrowServicesFunc)
+arrows[1].addEventListener('click', arrowDifferentialsFunc)
