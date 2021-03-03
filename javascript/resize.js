adf04b75c1842fcd357b3e5827a73cbcf4d1b797
Michel França <fr.mich17@gmail.com>
2021-03-03 04:37:32 -0300

    Remoção de um bloco de código não necessário

--- javascript/resize.js
+++ javascript/resize.js
@@ -4,12 +4,10 @@ window.addEventListener('resize', () => { // Quando a resolução for alterada d
     // Verifica se a largura é menor ou igual a 768px, se for ele adiciona um break no <span> contido na classe armazenada
     // na variável acima, se não ele mantem o textContent.
     screen.width <= 768 ? text.innerHTML = `<br>${text.textContent}<br>` : text.innerHTML = text.textContent
-
-    if (screen.width <= 768) {
-        navMenu.classList.remove('move-2')
-    }
+    
     if (screen.width >= 768) {
         navMenu.classList.remove('move')
+        navMenu.classList.remove('move-2')
         btnMenu.classList.remove('open')
         arrowServices.classList.remove('display-visible')
         arrowDifferentials.classList.remove('display-visible')
